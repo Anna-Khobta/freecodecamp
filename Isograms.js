6c55494571293fbbe83039e711adf25aa86e87ad
Anna-Khobta <ana14i88@gmail.com>
2023-09-04 15:54:28 +0300

    Create Isograms.js

---
+++ Isograms.js
@@ -0,0 +1,18 @@
+
+function isIsogram(str) {
+  if (str.isEmpty) {
+    return true;
+  } else {
+    str = str.toLowerCase();
+  }
+  var array = str.split('');
+  var sortedArr = array.slice().sort();
+
+  for (var i = 0; i < array.length; i++) {
+    //if duplicate is found return false.
+    if (sortedArr[i + 1] == sortedArr[i]) {
+      return false;
+    }
+  }
+  return true;
+}
